b8d81ba88061849b6dfc00324a8dc2183f407c1b
Dmytro Krapyvianskyi <dimka670020040@gmail.com>
2024-01-06 01:07:20 +0200

    header: design + showing + routerRoute + active

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faBell, faEnvelope, faUser } from '@fortawesome/free-solid-svg-icons';
+import { SearchInputComponent } from '../search-input/search-input.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule, RouterModule, SearchInputComponent],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
-
+  faBell = faBell;
+  faUser = faUser;
+  faEnvelope = faEnvelope;
 }
