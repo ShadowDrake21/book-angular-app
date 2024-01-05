e9e2fcee8bf62f40584dac546963115ec866759e
Dmytro Krapyvianskyi <dimka670020040@gmail.com>
2024-01-05 11:28:04 +0200

    addition to routes + header basic html

---
+++ src/app/pages/userlist-item/userlist-item.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-userlist-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './userlist-item.component.html',
+  styleUrl: './userlist-item.component.scss'
+})
+export class UserlistItemComponent {
+
+}
