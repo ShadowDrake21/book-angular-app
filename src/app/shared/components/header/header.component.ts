874dc681d0241ed1d6449b34a11b5075fe213ce0
Dmytro Krapyvianskyi <dimka670020040@gmail.com>
2024-01-06 23:05:17 +0200

    dropdown block without styles

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -3,11 +3,19 @@ import { RouterModule } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faBell, faEnvelope, faUser } from '@fortawesome/free-solid-svg-icons';
 import { SearchInputComponent } from '../search-input/search-input.component';
+import { ClickOutsideDirective } from './directives/click-outside.directive';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [FontAwesomeModule, RouterModule, SearchInputComponent],
+  imports: [
+    CommonModule,
+    FontAwesomeModule,
+    RouterModule,
+    SearchInputComponent,
+    ClickOutsideDirective,
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
@@ -15,4 +23,16 @@ export class HeaderComponent {
   faBell = faBell;
   faUser = faUser;
   faEnvelope = faEnvelope;
+
+  isMenuOpened: boolean = false;
+  clickedLi: string = 'background-color: rgb(122, 122, 122); color: #fff;';
+
+  toggleMenu(): void {
+    this.isMenuOpened = !this.isMenuOpened;
+  }
+
+  clickedOutside(): void {
+    this.isMenuOpened = false;
+    console.log('outside');
+  }
 }
