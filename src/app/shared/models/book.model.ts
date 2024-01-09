c3b5845c2f82a51fa9dc9cca0fbddf124413e36d
Dmytro Krapyvianskyi <dimka670020040@gmail.com>
2024-01-10 00:14:16 +0200

    fetching of books and basic showing

---
+++ src/app/shared/models/book.model.ts
@@ -0,0 +1,79 @@
+export interface Book {
+  kind: string;
+  id: string;
+  etag: string;
+  selfLink: string;
+  volumeInfo: VolumeInfo;
+  saleInfo: SaleInfo;
+  accessInfo: AccessInfo;
+}
+
+export interface VolumeInfo {
+  title: string;
+  authors: string[];
+  publisher: string;
+  publishedDate: string;
+  industryIdentifiers: IndustryIdentifier[];
+  readingModes: ReadingModes;
+  pageCount: number;
+  printType: string;
+  averageRating: number;
+  ratingsCount: number;
+  maturityRating: string;
+  allowAnonLogging: boolean;
+  contentVersion: string;
+  panelizationSummary: PanelizationSummary;
+  imageLinks: ImageLinks;
+  language: string;
+  previewLink: string;
+  infoLink: string;
+  canonicalVolumeLink: string;
+  description: string;
+}
+
+export interface IndustryIdentifier {
+  type: string;
+  identifier: string;
+}
+
+export interface ReadingModes {
+  text: boolean;
+  image: boolean;
+}
+
+export interface PanelizationSummary {
+  containsEpubBubbles: boolean;
+  containsImageBubbles: boolean;
+}
+
+export interface ImageLinks {
+  smallThumbnail: string;
+  thumbnail: string;
+}
+
+export interface SaleInfo {
+  country: string;
+  saleability: string;
+  isEbook: boolean;
+}
+
+export interface AccessInfo {
+  country: string;
+  viewability: string;
+  embeddable: boolean;
+  publicDomain: boolean;
+  textToSpeechPermission: string;
+  epub: Epub;
+  pdf: Pdf;
+  webReaderLink: string;
+  accessViewStatus: string;
+  quoteSharingAllowed: boolean;
+}
+
+export interface Epub {
+  isAvailable: boolean;
+}
+
+export interface Pdf {
+  isAvailable: boolean;
+}
