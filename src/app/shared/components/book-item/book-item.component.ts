46c9b4186bab8cb099d358cdc92fb299743140b0
Dmytro Krapyvianskyi <dimka670020040@gmail.com>
2024-01-10 11:06:51 +0200

    book-item component: basics

--- src/app/shared/components/book-item/book-item.component.ts
+++ src/app/shared/components/book-item/book-item.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Book } from '../../models/book.model';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-book-item',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './book-item.component.html',
-  styleUrl: './book-item.component.scss'
+  styleUrl: './book-item.component.scss',
 })
 export class BookItemComponent {
-
+  @Input({ required: true }) book!: Book;
 }
