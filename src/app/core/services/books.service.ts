e7766cb423f5a587120e7079f17dc2010129ee87
Dmytro Krapyvianskyi <dimka670020040@gmail.com>
2024-01-12 00:51:29 +0200

    carousel + getBooksBySubject

--- src/app/core/services/books.service.ts
+++ src/app/core/services/books.service.ts
@@ -1,6 +1,13 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
-import { SEARCH_API_URL } from '../constants/books.constants';
+import { SEARCH_API_URL, SUBJECT_API_URL } from '../constants/books.constants';
+
+export interface BooksBySubject {
+  details: boolean;
+  limit: number;
+  lang: string;
+  sort: string;
+}
 
 @Injectable({
   providedIn: 'root',
@@ -8,9 +15,20 @@ import { SEARCH_API_URL } from '../constants/books.constants';
 export class BooksService {
   http = inject(HttpClient);
 
-  getBooksByTitles(searchStr: string) {
-    return this.http.get<any>(
-      SEARCH_API_URL + '?title=' + searchStr + '&limit=20'
-    );
+  getBooksByTitles(searchStr: string, limit: number) {
+    return this.http.get<any>(SEARCH_API_URL + '?title=' + searchStr, {
+      params: { limit: limit },
+    });
+  }
+
+  getBooksBySubject(subject: string, paramValues: BooksBySubject) {
+    let httpParams = new HttpParams()
+      .append('details', paramValues.details)
+      .append('limit', paramValues.limit)
+      .append('lang', paramValues.lang)
+      .append('sort', paramValues.sort);
+    return this.http.get<any>(SUBJECT_API_URL + subject + '.json', {
+      params: httpParams,
+    });
   }
 }
