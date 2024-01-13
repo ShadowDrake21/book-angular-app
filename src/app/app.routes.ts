08f0d04e3fdf4b00227ba6122308b378eca5b304
Dmytro Krapyvianskyi <dimka670020040@gmail.com>
2024-01-13 10:41:55 +0200

    authorlist: basic http query + fetching + html

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -17,7 +17,7 @@ export const routes: Routes = [
       ),
   },
   {
-    path: 'booklist/:bookid',
+    path: 'booklist-item/:bookid',
     loadComponent: () =>
       import('./pages/booklist-item/booklist-item.component').then(
         (m) => m.BooklistItemComponent
@@ -35,6 +35,20 @@ export const routes: Routes = [
     loadComponent: () =>
       import('./pages/userlist-item/userlist-item.component').then(
         (m) => m.UserlistItemComponent
+      ),
+  },
+  {
+    path: 'authorlist',
+    loadComponent: () =>
+      import('./pages/authorlist/authorlist.component').then(
+        (m) => m.AuthorlistComponent
+      ),
+  },
+  {
+    path: 'authorlist/:authorid',
+    loadComponent: () =>
+      import('./pages/authorlist-item/authorlist-item.component').then(
+        (m) => m.AuthorlistItemComponent
       ),
   },
   {
