b87bd47caa681fd8c63d9f7446e5596d274e688c
Dmytro Krapyvianskyi <dimka670020040@gmail.com>
2024-01-15 13:10:41 +0200

    book-filter: basic html + basic styling for booklist

--- src/app/pages/booklist/components/booklist-filter/booklist-filter.component.ts
+++ src/app/pages/booklist/components/booklist-filter/booklist-filter.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-booklist-filter',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './booklist-filter.component.html',
-  styleUrl: './booklist-filter.component.scss'
+  styleUrl: './booklist-filter.component.scss',
 })
 export class BooklistFilterComponent {
-
+  faStar = faStar;
 }
