8d4c8f713c08ab92c41a14c011f5966af2a4e3fb
Dmytro Krapyvianskyi <dimka670020040@gmail.com>
2024-01-15 10:22:11 +0200

    footer is done!

---
+++ src/app/pages/about-developer/about-developer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about-developer',
+  standalone: true,
+  imports: [],
+  templateUrl: './about-developer.component.html',
+  styleUrl: './about-developer.component.scss'
+})
+export class AboutDeveloperComponent {
+
+}
