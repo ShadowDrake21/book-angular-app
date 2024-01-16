7a3981931a6651cf2e155551e06034bd0bcbdaba
Dmytro Krapyvianskyi <dimka670020040@gmail.com>
2024-01-16 23:06:31 +0200

    filter: updated data + basic form functionality

--- src/app/pages/booklist/components/booklist-filter/content/booklist-filter.content.ts
+++ src/app/pages/booklist/components/booklist-filter/content/booklist-filter.content.ts
@@ -1,13 +1,6 @@
 import { IBooklistFilter } from '../models/booklist-filter.model';
 
 export const BooklistFilterContent: IBooklistFilter = {
-  rating: [
-    { id: 'fiveStars', starsNumber: 5 },
-    { id: 'fourStars', starsNumber: 4 },
-    { id: 'threeStars', starsNumber: 3 },
-    { id: 'twoStars', starsNumber: 2 },
-    { id: 'oneStar', starsNumber: 1 },
-  ],
   genres: [
     {
       text: 'drama',
@@ -31,13 +24,5 @@ export const BooklistFilterContent: IBooklistFilter = {
     { text: 'war' },
     { text: 'death' },
     { text: 'comedy' },
-  ],
-  year: [
-    { num: 2024 },
-    { num: 2023 },
-    { num: 2022 },
-    { num: 2021 },
-    { num: 2020 },
-    { num: 2019 },
   ],
 };
