8623b65b2410b767657d5a49d10784a45c3ded3c
Dmytro Krapyvianskyi <dimka670020040@gmail.com>
2024-01-16 20:58:06 +0200

    filter: styles

--- src/app/pages/booklist/components/booklist-filter/booklist-filter.component.ts
+++ src/app/pages/booklist/components/booklist-filter/booklist-filter.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faStar } from '@fortawesome/free-solid-svg-icons';
+import { ButtonComponent } from '../../../../shared/components/UI/button/button.component';
 
 @Component({
   selector: 'app-booklist-filter',
   standalone: true,
-  imports: [FontAwesomeModule],
+  imports: [FontAwesomeModule, ButtonComponent],
   templateUrl: './booklist-filter.component.html',
   styleUrl: './booklist-filter.component.scss',
 })
