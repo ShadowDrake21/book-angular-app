b88dc4a8136c7e6f958a4cc779a681062f3270a1
Dmytro Krapyvianskyi <dimka670020040@gmail.com>
2024-01-16 23:06:31 +0200

    filter: updated data + basic form functionality

--- src/app/pages/booklist/components/booklist-filter/booklist-filter.component.ts
+++ src/app/pages/booklist/components/booklist-filter/booklist-filter.component.ts
@@ -1,25 +1,38 @@
 import { Component } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faStar } from '@fortawesome/free-solid-svg-icons';
 import { ButtonComponent } from '../../../../shared/components/UI/button/button.component';
 import { BooklistFilterContent } from './content/booklist-filter.content';
 import { IBooklistFilter } from './models/booklist-filter.model';
 import { CommonModule } from '@angular/common';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-booklist-filter',
   standalone: true,
-  imports: [CommonModule, FontAwesomeModule, ButtonComponent],
+  imports: [
+    CommonModule,
+    FontAwesomeModule,
+    ButtonComponent,
+    ReactiveFormsModule,
+  ],
   templateUrl: './booklist-filter.component.html',
   styleUrl: './booklist-filter.component.scss',
 })
 export class BooklistFilterComponent {
-  [x: string]: any;
-  faStar = faStar;
-
   filterContent: IBooklistFilter = BooklistFilterContent;
 
-  generateStarsArray(starsNumber: number): number[] {
-    return Array(starsNumber);
+  filterForm = new FormGroup({
+    author: new FormControl(''),
+    genre: new FormControl('0', Validators.required),
+    year: new FormControl(null),
+  });
+
+  onSubmit() {
+    console.log(this.filterForm);
   }
 }
--- src/app/pages/booklist/components/booklist-filter/models/booklist-filter.model.ts
+++ src/app/pages/booklist/components/booklist-filter/models/booklist-filter.model.ts
@@ -1,12 +1,5 @@
 export interface IBooklistFilter {
-  rating: {
-    id: string;
-    starsNumber: number;
-  }[];
   genres: {
     text: string;
-  }[];
-  year: {
-    num: number;
   }[];
 }
