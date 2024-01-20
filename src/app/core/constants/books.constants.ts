5bd1a404459297263ed121a054b84e209ee9edd4
Dmytro Krapyvianskyi <dimka670020040@gmail.com>
2024-01-20 15:23:10 +0200

    book-item: correct url per book

--- src/app/core/constants/books.constants.ts
+++ src/app/core/constants/books.constants.ts
@@ -3,4 +3,4 @@ export const SUBJECT_API_URL: string = 'https://openlibrary.org/subjects/';
 export const AUTHORS_API_URL: string =
   'https://openlibrary.org/search/authors.json';
 export const IMG_URL: string = 'https://covers.openlibrary.org/b/olid/';
-export const WORK_URL: string = 'https://openlibrary.org/works';
+export const WORK_URL: string = 'https://openlibrary.org/works/';
--- src/app/pages/booklist-item/booklist-item.component.ts
+++ src/app/pages/booklist-item/booklist-item.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { BooksService } from '../../core/services/books.service';
 
 @Component({
   selector: 'app-booklist-item',
   standalone: true,
   imports: [],
   templateUrl: './booklist-item.component.html',
-  styleUrl: './booklist-item.component.scss'
+  styleUrl: './booklist-item.component.scss',
 })
-export class BooklistItemComponent {
-
+export class BooklistItemComponent implements OnInit {
+  booksService = inject(BooksService);
+  ngOnInit(): void {}
 }
