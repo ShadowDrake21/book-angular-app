21ad4093c43eecaac32ffe90a570a4c3e43643a3
Dmytro Krapyvianskyi <dimka670020040@gmail.com>
2024-01-20 16:08:30 +0200

    image pipe feature

--- src/app/core/constants/books.constants.ts
+++ src/app/core/constants/books.constants.ts
@@ -2,5 +2,7 @@ export const SEARCH_API_URL: string = 'https://openlibrary.org/search.json';
 export const SUBJECT_API_URL: string = 'https://openlibrary.org/subjects/';
 export const AUTHORS_API_URL: string =
   'https://openlibrary.org/search/authors.json';
-export const IMG_URL: string = 'https://covers.openlibrary.org/b/olid/';
 export const WORK_URL: string = 'https://openlibrary.org/works/';
+
+export const IMG_OLID_URL: string = 'https://covers.openlibrary.org/b/olid/';
+export const IMG_ID_URL: string = 'https://covers.openlibrary.org/b/id/';
--- src/app/pages/booklist-item/booklist-item.component.ts
+++ src/app/pages/booklist-item/booklist-item.component.ts
@@ -2,11 +2,12 @@ import { Component, OnInit, inject } from '@angular/core';
 import { BooksService } from '../../core/services/books.service';
 import { ActivatedRoute } from '@angular/router';
 import { IWork } from '../../shared/models/book.model';
+import { BookImagePipe } from '../../shared/pipes/book-image.pipe';
 
 @Component({
   selector: 'app-booklist-item',
   standalone: true,
-  imports: [],
+  imports: [BookImagePipe],
   templateUrl: './booklist-item.component.html',
   styleUrl: './booklist-item.component.scss',
 })
@@ -16,11 +17,14 @@ export class BooklistItemComponent implements OnInit {
 
   path!: string;
   book!: IWork;
+
+  mainCover!: string;
   ngOnInit(): void {
     this.path = this.route.snapshot.url[1].path;
 
     this.booksService.getWorkByKey(this.path).subscribe((res) => {
       this.book = res;
+      this.mainCover = this.book.covers[0].toString();
       console.log(this.book);
     });
   }
