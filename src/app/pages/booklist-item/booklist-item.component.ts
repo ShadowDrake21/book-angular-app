269901354492e6acf7d309c6716f3b8f3d5c36ca
Dmytro Krapyvianskyi <dimka670020040@gmail.com>
2024-01-20 16:58:33 +0200

    booklist-item: basic structure

--- src/app/pages/booklist-item/booklist-item.component.ts
+++ src/app/pages/booklist-item/booklist-item.component.ts
@@ -3,11 +3,12 @@ import { BooksService } from '../../core/services/books.service';
 import { ActivatedRoute } from '@angular/router';
 import { IWork } from '../../shared/models/book.model';
 import { BookImagePipe } from '../../shared/pipes/book-image.pipe';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-booklist-item',
   standalone: true,
-  imports: [BookImagePipe],
+  imports: [CommonModule, BookImagePipe],
   templateUrl: './booklist-item.component.html',
   styleUrl: './booklist-item.component.scss',
 })
@@ -16,16 +17,20 @@ export class BooklistItemComponent implements OnInit {
   route = inject(ActivatedRoute);
 
   path!: string;
-  book!: IWork;
+  loadingBook?: boolean;
 
+  book!: IWork;
   mainCover!: string;
+
   ngOnInit(): void {
     this.path = this.route.snapshot.url[1].path;
 
+    this.loadingBook = true;
     this.booksService.getWorkByKey(this.path).subscribe((res) => {
       this.book = res;
       this.mainCover = this.book.covers[0].toString();
       console.log(this.book);
+      this.loadingBook = false;
     });
   }
 }
