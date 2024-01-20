d2a7d5bdd30471d638e521fa9f459a89d5e3fde1
Dmytro Krapyvianskyi <dimka670020040@gmail.com>
2024-01-20 15:45:22 +0200

    booklist-item: fetch a work

--- src/app/pages/booklist-item/booklist-item.component.ts
+++ src/app/pages/booklist-item/booklist-item.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { BooksService } from '../../core/services/books.service';
+import { ActivatedRoute } from '@angular/router';
+import { IWork } from '../../shared/models/book.model';
 
 @Component({
   selector: 'app-booklist-item',
@@ -10,5 +12,16 @@ import { BooksService } from '../../core/services/books.service';
 })
 export class BooklistItemComponent implements OnInit {
   booksService = inject(BooksService);
-  ngOnInit(): void {}
+  route = inject(ActivatedRoute);
+
+  path!: string;
+  book!: IWork;
+  ngOnInit(): void {
+    this.path = this.route.snapshot.url[1].path;
+
+    this.booksService.getWorkByKey(this.path).subscribe((res) => {
+      this.book = res;
+      console.log(this.book);
+    });
+  }
 }
