ce65d3a6404e58df9038c4211574368925e184dc
Dmytro Krapyvianskyi <dimka670020040@gmail.com>
2024-01-25 00:19:27 +0200

    review form is done!

--- src/app/shared/models/comment.model.ts
+++ src/app/shared/models/comment.model.ts
@@ -1,6 +1,7 @@
 import { Timestamp } from '@angular/fire/firestore';
 
 export interface IBookCommentToDB {
+  id: string;
   email: string;
   comment: string;
   rating: number;
@@ -9,11 +10,24 @@ export interface IBookCommentToDB {
 }
 
 export interface IBookCommentToClient {
+  id: string;
   email: string;
   comment: string;
   rating: number;
   date: Date;
   photoURL: string;
+}
+
+// export interface IUserCommentOnBook {
+//   bookId: string;
+//   comment: string;
+//   rating: number;
+//   date: Date;
+// }
+
+export interface ICommentResult {
+  isSuccessfull: boolean;
+  message: string;
 }
 
 export interface INeededUserInfo {
