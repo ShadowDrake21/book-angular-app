6dd44497a32fc1a174b32b37f41ad0590c5fdc27
Dmytro Krapyvianskyi <dimka670020040@gmail.com>
2024-01-26 11:49:15 +0200

    refactoring: rating

--- src/app/pages/booklist-item/components/bookitem-ratings/bookitem-ratings.component.ts
+++ src/app/pages/booklist-item/components/bookitem-ratings/bookitem-ratings.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { IBookExternalInfo } from '../../../../shared/models/book.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-bookitem-ratings',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './bookitem-ratings.component.html',
-  styleUrl: './bookitem-ratings.component.scss'
+  styleUrl: './bookitem-ratings.component.scss',
 })
-export class BookitemRatingsComponent {
+export class BookitemRatingsComponent implements OnInit {
+  @Input({ required: true }) ratingData!: IBookExternalInfo;
 
+  ngOnInit(): void {
+    console.log('rating: ', this.ratingData);
+  }
 }
