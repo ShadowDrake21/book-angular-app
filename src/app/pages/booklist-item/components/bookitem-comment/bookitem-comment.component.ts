4f0d0daad598c26a4b12eb5fe5d5ace6448d91dc
Dmytro Krapyvianskyi <dimka670020040@gmail.com>
2024-01-26 12:53:51 +0200

    refactoring: comment-section and comment-item. Refactoring is done!

--- src/app/pages/booklist-item/components/bookitem-comment/bookitem-comment.component.ts
+++ src/app/pages/booklist-item/components/bookitem-comment/bookitem-comment.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { StarRatingModule } from 'angular-star-rating';
+import { IBookCommentToClient } from '../../../../shared/models/comment.model';
 
 @Component({
   selector: 'app-bookitem-comment',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, StarRatingModule],
   templateUrl: './bookitem-comment.component.html',
-  styleUrl: './bookitem-comment.component.scss'
+  styleUrl: './bookitem-comment.component.scss',
 })
 export class BookitemCommentComponent {
+  @Input({ required: true }) comment!: IBookCommentToClient;
+  @Input() isUserComment: boolean = false;
+  @Output() editCommentId = new EventEmitter<string>();
+  @Output() deleteCommentId = new EventEmitter<string>();
 
+  editComment() {
+    this.editCommentId.emit(this.comment.id);
+  }
+
+  deleteComment() {
+    this.deleteCommentId.emit(this.comment.id);
+  }
 }
