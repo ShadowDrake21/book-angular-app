389b22a91bc1d9162a7b7b7c8d4a17fa2470daeb
Dmytro <dimka670020040@gmail.com>
2024-01-30 22:14:51 +0200

    userlist: basic functionality + user item

--- src/app/pages/userlist/userlist.component.ts
+++ src/app/pages/userlist/userlist.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { AuthService } from '../../core/authentication/auth.service';
+import { UsersService } from '../../core/services/users.service';
+import { IUser } from '../../shared/models/user.model';
+import { UserItemComponent } from '../../shared/components/user-item/user-item.component';
 
 @Component({
   selector: 'app-userlist',
   standalone: true,
-  imports: [],
+  imports: [UserItemComponent],
   templateUrl: './userlist.component.html',
   styleUrl: './userlist.component.scss',
 })
-export class UserlistComponent {}
+export class UserlistComponent implements OnInit {
+  authService = inject(AuthService);
+  usersService = inject(UsersService);
+
+  users: IUser[] = [];
+  loadingUsers!: boolean;
+  ngOnInit(): void {
+    this.loadingUsers = true;
+    this.usersService.getAllUsers().then((res) => {
+      console.log('users', res);
+      this.users = res;
+      this.loadingUsers = false;
+    });
+  }
+}
