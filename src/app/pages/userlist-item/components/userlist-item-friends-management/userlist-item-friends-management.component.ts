0969cd122d2b892dd0fa8c33264323d6ba5c4731
Dmytro Krapyvianskyi <dimka670020040@gmail.com>
2024-02-03 14:57:52 +0200

    userlist-item: part 6, send and decline friend request

--- src/app/pages/userlist-item/components/userlist-item-friends-management/userlist-item-friends-management.component.ts
+++ src/app/pages/userlist-item/components/userlist-item-friends-management/userlist-item-friends-management.component.ts
@@ -1,12 +1,96 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input, OnDestroy, OnInit, inject } from '@angular/core';
+import { FriendsManagementService } from '../../../../core/services/friends-management.service';
+import { AuthService } from '../../../../core/authentication/auth.service';
+import { Subscription } from 'rxjs';
+import {
+  IGottenFriendRequestToDB,
+  ISentFriendRequestToDB,
+} from '../../../../shared/models/friendsManagement.model';
+import { Timestamp } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-userlist-item-friends-management',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './userlist-item-friends-management.component.html',
-  styleUrl: './userlist-item-friends-management.component.scss'
+  styleUrl: './userlist-item-friends-management.component.scss',
 })
-export class UserlistItemFriendsManagementComponent {
+export class UserlistItemFriendsManagementComponent
+  implements OnInit, OnDestroy
+{
+  authService = inject(AuthService);
+  friendsManagementService = inject(FriendsManagementService);
 
+  @Input({ required: true }) userEmail: string | null = '';
+
+  private subscription!: Subscription;
+  ourEmail: string = '';
+  isFriendRequestSent: boolean = false;
+  loadingOurEmail?: boolean;
+  loadingIsFriendRequestSent?: boolean;
+
+  // zrobić akceptację, uchylenie zapytu + stronę z główną informacją
+  ngOnInit(): void {
+    this.loadingOurEmail = true;
+    this.loadingIsFriendRequestSent = true;
+    this.subscription = this.authService.user$.subscribe(async (data) => {
+      if (!data?.email || !this.userEmail) return;
+      this.ourEmail = data?.email;
+      this.loadingOurEmail = false;
+      console.log('emails: ', this.userEmail, this.ourEmail);
+      console.log('this.loading our email');
+
+      await this.friendsManagementService
+        .checkUserSentOrGotFriendRequest(
+          'sentRequests',
+          this.ourEmail,
+          this.userEmail,
+          'recipientEmail'
+        )
+        .then((res: boolean) => {
+          this.isFriendRequestSent = res;
+          this.loadingIsFriendRequestSent = false;
+        });
+    });
+  }
+
+  async onFriendsRequest() {
+    if (!this.userEmail) return;
+    this.isFriendRequestSent = !this.isFriendRequestSent;
+    if (this.isFriendRequestSent) {
+      let date = Timestamp.now();
+      await this.friendsManagementService.sendFriendRequest(
+        'sentRequests',
+        this.ourEmail,
+        this.userEmail,
+        {
+          recipientEmail: this.userEmail,
+          date: date,
+        } as ISentFriendRequestToDB
+      );
+      await this.friendsManagementService.sendFriendRequest(
+        'gottenRequests',
+        this.userEmail,
+        this.ourEmail,
+        {
+          senderEmail: this.ourEmail,
+          date: date,
+        } as IGottenFriendRequestToDB
+      );
+    } else {
+      await this.friendsManagementService.deleteSentFriendRequest(
+        this.ourEmail,
+        this.userEmail
+      );
+      await this.friendsManagementService.deleteGottenFriendRequest(
+        this.ourEmail,
+        this.userEmail
+      );
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
