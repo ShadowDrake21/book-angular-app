10733b26d97bc867a2ebf9832a939caebd4649cd
Dmytro Krapyvianskyi <dimka670020040@gmail.com>
2024-02-03 11:54:22 +0200

    userlist-item: part 5, friends-managment service

---
+++ src/app/core/services/friends-management.service.ts
@@ -0,0 +1,161 @@
+import { Injectable, inject } from '@angular/core';
+import {
+  Firestore,
+  Timestamp,
+  collection,
+  deleteDoc,
+  doc,
+  getDocs,
+  query,
+  setDoc,
+  where,
+} from '@angular/fire/firestore';
+import {
+  IGottenFriendRequestToClient,
+  IGottenFriendRequestToDB,
+  ISentFriendRequestToClient,
+  ISentFriendRequestToDB,
+} from '../../shared/models/friendsManagement.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class FriendsManagementService {
+  private _firestore = inject(Firestore);
+
+  async sendFriendRequest(
+    entity: string,
+    senderEmail: string,
+    recipientEmail: string,
+    dataObj: ISentFriendRequestToDB | IGottenFriendRequestToDB
+  ) {
+    await setDoc(
+      doc(
+        this._firestore,
+        'friendsManagement',
+        senderEmail,
+        entity,
+        recipientEmail
+      ),
+      dataObj
+    ).then(() => {
+      this.sendFriendRequest('gottenRequests', recipientEmail, senderEmail, {
+        senderEmail,
+        date: dataObj.date,
+      } as IGottenFriendRequestToDB);
+    });
+  }
+
+  async checkUserSentOrGotFriendRequest(
+    entity: string,
+    emailA: string,
+    emailB: string,
+    queryParam: string
+  ): Promise<boolean> {
+    const querySnapshot = await getDocs(
+      query(
+        collection(this._firestore, 'friendsManagement', emailA, entity),
+        where(queryParam, '==', emailB)
+      )
+    );
+
+    return !querySnapshot.empty;
+  }
+
+  async getFriendRequest(
+    action: 'send' | 'get',
+    emailA: string,
+    emailB: string
+  ) {
+    let friendRequests: Array<
+      ISentFriendRequestToClient | IGottenFriendRequestToClient
+    > = [];
+    let querySnapshot;
+    if (action === 'send') {
+      querySnapshot = await getDocs(
+        query(
+          collection(
+            this._firestore,
+            'friendsManagement',
+            emailA,
+            'sentRequests'
+          ),
+          where('recipientEmail', '==', emailB)
+        )
+      );
+    } else {
+      querySnapshot = await getDocs(
+        query(
+          collection(
+            this._firestore,
+            'friendsManagement',
+            emailA,
+            'gottenRequests'
+          ),
+          where('senderEmail', '==', emailB)
+        )
+      );
+    }
+
+    querySnapshot.forEach((doc) => {
+      let requestDataFromDB;
+      if (action === 'send') {
+        requestDataFromDB = doc.data() as ISentFriendRequestToDB;
+
+        let transformDate: Date = (
+          requestDataFromDB.date as Timestamp
+        ).toDate();
+        const requestDataToClient: ISentFriendRequestToClient = {
+          recipientEmail: requestDataFromDB.recipientEmail,
+          date: transformDate,
+        };
+        friendRequests.push(requestDataToClient);
+      } else {
+        requestDataFromDB = doc.data() as IGottenFriendRequestToDB;
+
+        let transformDate: Date = (
+          requestDataFromDB.date as Timestamp
+        ).toDate();
+        const requestDataToClient: IGottenFriendRequestToClient = {
+          senderEmail: requestDataFromDB.senderEmail,
+          date: transformDate,
+        };
+        friendRequests.push(requestDataToClient);
+      }
+    });
+    console.log('get request:', friendRequests);
+    return friendRequests;
+  }
+
+  async deleteSentFriendRequest(
+    entity: string,
+    senderEmail: string,
+    recipientEmail: string
+  ) {
+    await deleteDoc(
+      doc(
+        this._firestore,
+        'friendsManagement',
+        senderEmail,
+        entity,
+        recipientEmail
+      )
+    );
+  }
+
+  async deleteGottenFriendRequest(
+    entity: string,
+    senderEmail: string,
+    recipientEmail: string
+  ) {
+    await deleteDoc(
+      doc(
+        this._firestore,
+        'friendsManagement',
+        recipientEmail,
+        entity,
+        senderEmail
+      )
+    );
+  }
+}
