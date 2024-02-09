7859e6ecbd83e53b019eba30fe86170225bdcbb7
Dmytro Krapyvianskyi <dimka670020040@gmail.com>
2024-02-09 13:40:19 +0200

    refactoring of favourite books & authors: services

--- src/app/pages/my-profile/components/profile-authors/profile-authors.component.ts
+++ src/app/pages/my-profile/components/profile-authors/profile-authors.component.ts
@@ -13,62 +13,32 @@ import { BookmarkService } from '../../../../core/services/bookmark.service';
 import { User } from '@angular/fire/auth';
 import { IAuthor } from '../../../../shared/models/author.model';
 import { forkJoin } from 'rxjs';
+import { FavouriteAuthorsService } from '../../../../core/services/favourite-authors.service';
 
 @Component({
   selector: 'app-profile-authors',
   standalone: true,
   imports: [CommonModule, AuthorItemComponent],
+  providers: [FavouriteAuthorsService],
   templateUrl: './profile-authors.component.html',
   styleUrl: './profile-authors.component.scss',
 })
 export class ProfileAuthorsComponent implements OnInit {
-  private authorsService = inject(AuthorsService);
-  private bookmarkService = inject(BookmarkService);
+  protected favouriteAuthorsService = inject(FavouriteAuthorsService);
 
   @Input({ required: true }) user!: User | null;
 
-  loadingAuthorBookmarks!: boolean;
-  userAuthorBookmarks: string[] = [];
-
-  loadingAuthors!: boolean;
-  userAuthors: IAuthor[] = [];
-
-  isAnyAuthor: boolean = true;
+  visibleAuthors: IAuthor[] = [];
 
   async ngOnInit(): Promise<void> {
-    this.loadingAuthorBookmarks = true;
-    this.loadingAuthors = true;
-    await this.loadingItems();
-  }
-
-  async loadingItems() {
-    await this.bookmarkService
-      .getAllBookmarks('authors', this.user?.email)
-      .then((bookmarks) => {
-        if (!bookmarks) return;
-        this.userAuthorBookmarks = bookmarks;
-      });
-    this.loadingAuthorBookmarks = false;
-
-    if (this.userAuthorBookmarks.length) {
-      this.isAnyAuthor = true;
-    } else {
-      this.isAnyAuthor = false;
-    }
-
-    const authorsObservables = this.userAuthorBookmarks.map(
-      (authorKey: string) => {
-        return this.authorsService.getAuthorByKey(authorKey);
-      }
-    );
-
-    forkJoin(authorsObservables).subscribe((authors: IAuthor[]) => {
-      authors.forEach((author: IAuthor) => {
-        author.key = author.key.slice(9, author.key.length);
-        this.userAuthors.push(author);
-      });
-      this.userAuthors = this.userAuthors.slice(0, 5);
-      this.loadingAuthors = false;
+    this.favouriteAuthorsService.userEmail = this.user?.email;
+    await this.favouriteAuthorsService.loadingItems().then(async () => {
+      await new Promise((resolve) => setTimeout(resolve, 1500));
+      this.favouriteAuthorsService.loadingAuthors = false;
+      this.visibleAuthors = this.favouriteAuthorsService.userAuthors.slice(
+        0,
+        5
+      );
     });
   }
 }
