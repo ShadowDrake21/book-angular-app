67472d65e81042199b00c179a913ac22e4d3b4ad
Dmytro Krapyvianskyi <dimka670020040@gmail.com>
2024-02-17 21:01:36 +0200

    my-reading-challenge-item: plus, minus, update, cancel. subtitle

--- src/app/pages/my-reading-challenges/components/reading-challenge-item/reading-challenge-item.component.ts
+++ src/app/pages/my-reading-challenges/components/reading-challenge-item/reading-challenge-item.component.ts
@@ -5,10 +5,12 @@ import {
   OnChanges,
   OnInit,
   SimpleChanges,
+  inject,
 } from '@angular/core';
 import { TruncateTextPipe } from '../../../../shared/pipes/truncate-text.pipe';
 import { IChallenge } from '../../../../shared/models/challenge.model';
 import { RouterModule } from '@angular/router';
+import { ChallengesService } from '../../../../core/services/challenges.service';
 
 @Component({
   selector: 'app-reading-challenge-item',
@@ -17,13 +19,26 @@ import { RouterModule } from '@angular/router';
   templateUrl: './reading-challenge-item.component.html',
   styleUrl: './reading-challenge-item.component.scss',
 })
-export class ReadingChallengeItemComponent implements OnInit, OnChanges {
+export class ReadingChallengeItemComponent implements OnInit {
+  private challengesService = inject(ChallengesService);
   @Input({ required: true }) challenge!: IChallenge;
+  @Input({ required: true }) email!: string | null | undefined;
+
   progressBarWidth!: string;
   progressBarTitle!: string;
+  subtitle: string = 'Start the challenge!';
+
+  isEdit: boolean = false;
+  oldReadCount!: number;
 
   ngOnInit(): void {
     console.log('challenge:', this.challenge);
+    this.oldReadCount = this.challenge.read;
+    this.updateProgressBar();
+    this.subtitleChange();
+  }
+
+  updateProgressBar() {
     this.progressBarWidth =
       'width:' +
       Math.round((this.challenge.read / this.challenge.total) * 100) +
@@ -31,5 +46,53 @@ export class ReadingChallengeItemComponent implements OnInit, OnChanges {
     this.progressBarTitle =
       Math.round((this.challenge.read / this.challenge.total) * 100) + '%';
   }
-  ngOnChanges(changes: SimpleChanges): void {}
+
+  minusBook() {
+    if (!this.isEdit) this.isEdit = true;
+    this.challenge.read--;
+    console.log(`You have read ${this.challenge.read}`);
+    this.updateProgressBar();
+    this.subtitleChange();
+  }
+
+  plusBook() {
+    if (!this.isEdit) this.isEdit = true;
+    this.challenge.read++;
+    console.log(`You have read ${this.challenge.read}`);
+    this.updateProgressBar();
+    this.subtitleChange();
+  }
+
+  async updateChallenge() {
+    if (this.oldReadCount === this.challenge.read || !this.email) {
+      return;
+    }
+    this.isEdit = false;
+    await this.challengesService.updateChallenge(
+      this.email,
+      this.challenge.id,
+      this.challenge
+    );
+    this.oldReadCount = this.challenge.read;
+  }
+
+  subtitleChange() {
+    if (this.challenge.read === this.challenge.total) {
+      this.subtitle = "Congrats! You've done it!";
+    } else if (
+      this.challenge.read > 0 &&
+      this.challenge.read < this.challenge.total
+    ) {
+      this.subtitle = "You're on track!";
+    } else {
+      this.subtitle = 'Start the challenge!';
+    }
+  }
+
+  cancelEdit() {
+    this.isEdit = false;
+    this.challenge.read = this.oldReadCount;
+    this.updateProgressBar();
+    this.subtitleChange();
+  }
 }
