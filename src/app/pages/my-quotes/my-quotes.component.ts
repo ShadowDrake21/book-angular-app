d116a5105ab42b60cdfe428b2b2f4698cacda5ee
Dmytro Krapyvianskyi <dimka670020040@gmail.com>
2024-02-19 23:36:04 +0200

    quotesForm: UI

--- src/app/pages/my-quotes/my-quotes.component.ts
+++ src/app/pages/my-quotes/my-quotes.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { QuotesListComponent } from './components/quotes-list/quotes-list.component';
+import { QuotesService } from '../../core/services/quotes.service';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { ButtonComponent } from '../../shared/components/UI/button/button.component';
 
 @Component({
   selector: 'app-my-quotes',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    QuotesListComponent,
+    ReactiveFormsModule,
+    ButtonComponent,
+  ],
   templateUrl: './my-quotes.component.html',
-  styleUrl: './my-quotes.component.scss'
+  styleUrl: './my-quotes.component.scss',
 })
 export class MyQuotesComponent {
+  private quotesService = inject(QuotesService);
 
+  quoteForm = new FormGroup({
+    text: new FormControl('', Validators.required),
+    author: new FormControl('', Validators.required),
+  });
+
+  onQuoteSubmit() {
+    console.log(this.quoteForm.value);
+  }
 }
