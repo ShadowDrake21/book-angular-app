802406ff8358a7312af889f4150ddc1b6dd850cb
Dmytro Krapyvianskyi <dimka670020040@gmail.com>
2024-02-20 23:41:07 +0200

    genres service: basics

---
+++ src/app/core/services/genres.service.ts
@@ -0,0 +1,80 @@
+import { Injectable, inject } from '@angular/core';
+import {
+  Firestore,
+  collection,
+  deleteDoc,
+  doc,
+  getDocs,
+  orderBy,
+  query,
+  setDoc,
+  updateDoc,
+  where,
+} from '@angular/fire/firestore';
+import { IQuote } from '../../shared/models/quote.model';
+import { IGenre } from '../../shared/models/genre.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class GenresService {
+  private _firestore = inject(Firestore);
+
+  async getAllGenres(email: string): Promise<IGenre[]> {
+    let genres: Array<IGenre> = [];
+    const querySnapshot = await getDocs(
+      query(
+        collection(this._firestore, 'usersData', email, 'genres'),
+        orderBy('name', 'asc')
+      )
+    );
+
+    querySnapshot.forEach((doc) => {
+      const genreFromDB = doc.data() as IGenre;
+      genres.push(genreFromDB);
+    });
+    return genres;
+  }
+
+  async getGenre(email: string, genreId: string): Promise<IGenre[]> {
+    let genres: Array<IGenre> = [];
+    const querySnapshot = await getDocs(
+      query(
+        collection(this._firestore, 'usersData', email, 'genres'),
+        where('id', '==', genreId)
+      )
+    );
+
+    querySnapshot.forEach((doc) => {
+      const genreFromDB = doc.data() as IGenre;
+      genres.push(genreFromDB);
+    });
+    return genres;
+  }
+
+  async addGenresGroup(email: string, genres: IGenre[]) {
+    for (const genre of genres) {
+      await this.addNewGenre(email, genre);
+    }
+  }
+
+  async addNewGenre(email: string, genre: IGenre) {
+    await setDoc(
+      doc(this._firestore, 'usersData', email, 'genres', genre.id),
+      genre
+    );
+  }
+
+  async deleteGenre(email: string, genreId: string) {
+    await deleteDoc(doc(this._firestore, 'userData', email, 'genres', genreId));
+  }
+
+  async updateGenre(email: string, genreId: string, dataObj: IGenre) {
+    const docRef = doc(this._firestore, 'usersData', email, 'genres', genreId);
+    let updateObj: object = {
+      id: dataObj.id,
+      name: dataObj.name,
+    };
+    await updateDoc(docRef, updateObj);
+  }
+}
