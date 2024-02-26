0e2e9d137f743c627d5b9563d8ce38cfd8808572
Dmytro Krapyvianskyi <dimka670020040@gmail.com>
2024-02-26 15:10:31 +0200

    contact me form: UI

--- src/app/pages/about-developer/about-developer.component.ts
+++ src/app/pages/about-developer/about-developer.component.ts
@@ -1,12 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { InputComponent } from '../../shared/components/UI/input/input.component';
+import { ButtonComponent } from '../../shared/components/UI/button/button.component';
 
 @Component({
   selector: 'app-about-developer',
   standalone: true,
   imports: [],
   templateUrl: './about-developer.component.html',
-  styleUrl: './about-developer.component.scss'
+  styleUrl: './about-developer.component.scss',
 })
-export class AboutDeveloperComponent {
-
-}
+export class AboutDeveloperComponent {}
--- src/app/pages/contact-me/contact-me.component.ts
+++ src/app/pages/contact-me/contact-me.component.ts
@@ -1,12 +1,38 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { ButtonComponent } from '../../shared/components/UI/button/button.component';
+import { InputComponent } from '../../shared/components/UI/input/input.component';
+import { TextareaComponent } from '../../shared/components/UI/textarea/textarea.component';
 
 @Component({
   selector: 'app-contact-me',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    InputComponent,
+    TextareaComponent,
+    ButtonComponent,
+  ],
   templateUrl: './contact-me.component.html',
-  styleUrl: './contact-me.component.scss'
+  styleUrl: './contact-me.component.scss',
 })
 export class ContactMeComponent {
+  contactForm = new FormGroup({
+    email: new FormControl('', Validators.email),
+    name: new FormControl(''),
+    message: new FormControl('', Validators.minLength(10)),
+  });
 
+  myEmail: string = 'dimka670020040@gmail.com';
+
+  onSubmit() {
+    console.log('form', this.contactForm);
+  }
 }
