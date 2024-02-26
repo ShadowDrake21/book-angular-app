e314e96992ac88077c1ad33fffffb05bde1cde21
Dmytro Krapyvianskyi <dimka670020040@gmail.com>
2024-02-26 14:02:14 +0200

    your activity on home page is done!

--- src/app/pages/home/components/about-user/about-user.component.ts
+++ src/app/pages/home/components/about-user/about-user.component.ts
@@ -7,6 +7,11 @@ import { IUpdateProfile } from '../../../../shared/models/profileManipulations.m
 import { aboutUser } from '../../mocks/home.mocks';
 import { UsersService } from '../../../../core/services/users.service';
 import { IUser } from '../../../../shared/models/user.model';
+import { IActivity } from '../../../../shared/models/activity.model';
+import { AuthorsService } from '../../../../core/services/authors.service';
+import { BookmarkService } from '../../../../core/services/bookmark.service';
+import { CommentsService } from '../../../../core/services/comments.service';
+import { FriendsManagementService } from '../../../../core/services/friends-management.service';
 
 @Component({
   selector: 'app-about-user',
@@ -16,24 +21,72 @@ import { IUser } from '../../../../shared/models/user.model';
   styleUrl: './about-user.component.scss',
 })
 export class AboutUserComponent implements OnInit {
-  authService = inject(AuthService);
-  booksService = inject(BooksService);
-  usersService = inject(UsersService);
+  private authService = inject(AuthService);
+  private bookmarkService = inject(BookmarkService);
+  private commentsService = inject(CommentsService);
+  private friendsManagementService = inject(FriendsManagementService);
 
-  user!: User | null;
   loadingUser!: boolean;
+  user!: User | null;
 
-  aboutUser = aboutUser;
+  loadingAboutUser!: boolean;
+  aboutUser!: IActivity;
 
   async ngOnInit(): Promise<void> {
     this.loadingUser = true;
-    this.getUserInfo();
+    this.loadingAboutUser = true;
+    await this.getUserInfo();
   }
 
-  getUserInfo() {
-    this.authService.user$.subscribe((res) => {
-      this.user = res;
-      this.loadingUser = false;
+  async getUserInfo() {
+    return new Promise<void>((resolve) => {
+      this.authService.user$.subscribe((res) => {
+        this.user = res;
+        this.loadingUser = false;
+
+        if (this.user?.email) {
+          this.formAboutUser().then(() => {
+            this.loadingAboutUser = false;
+            resolve();
+          });
+        } else {
+          resolve();
+        }
+      });
     });
+  }
+
+  async formAboutUser() {
+    if (!this.user?.email) return;
+
+    const [
+      booksBookmarks,
+      authorsBookmarks,
+      booksComments,
+      authorsComments,
+      friendsRequests,
+    ] = await Promise.all([
+      this.bookmarkService.getAllBookmarksInUserData(this.user.email, 'books'),
+      this.bookmarkService.getAllBookmarksInUserData(
+        this.user.email,
+        'authors'
+      ),
+      this.commentsService.getAllCommentsInUserData(this.user.email, 'books'),
+      this.commentsService.getAllCommentsInUserData(this.user.email, 'authors'),
+      this.friendsManagementService.getAllGottenFriendRequests(
+        this.user.email,
+        'gottenRequests'
+      ),
+    ]);
+
+    this.aboutUser = {
+      likedBooks: booksBookmarks.length,
+      likedAuthors: authorsBookmarks.length,
+      writtenReviewsBooks: booksComments.length,
+      writtenReviewsAuthors: authorsComments.length,
+      notifications: friendsRequests.length,
+    };
+    console.log('aboutUser', this.aboutUser);
+    this.loadingAboutUser = false;
   }
 }
